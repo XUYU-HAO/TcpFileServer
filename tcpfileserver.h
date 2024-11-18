d8801a43228d4377814e72d63464d1ae1b71a7c8
XUYU-HAO <41243132@nfu.edu.tw>
2024-11-18 14:40:47 +0800

    addnewipandadds

--- tcpfileserver.h
+++ tcpfileserver.h
@@ -2,36 +2,45 @@
 #define TCPFILESERVER_H
 
 #include <QDialog>
-#include <QtNetwork>
-#include <QtWidgets>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QFile>
+#include <QProgressBar>
+#include <QLabel>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QDialogButtonBox>
 
 class TcpFileServer : public QDialog
 {
     Q_OBJECT
 
 public:
-    TcpFileServer(QWidget *parent = 0);
+    explicit TcpFileServer(QWidget *parent = nullptr);
     ~TcpFileServer();
-public slots:
-    void start();
-    void acceptConnection();
-    void updateServerProgress();
-    void displayError(QAbstractSocket::SocketError socketError);
-private:
-    QProgressBar     *serverProgressBar;
-    QLabel           *serverStatusLabel;
-    QPushButton      *startButton;
-    QPushButton      *quitButton;
-    QDialogButtonBox *buttonBox;
 
-    QTcpServer       tcpServer;
-    QTcpSocket       *tcpServerConnection;
-    qint64           totalBytes;
-    qint64           byteReceived;
-    qint64           fileNameSize;
-    QString          fileName;
-    QFile            *localFile;
-    QByteArray       inBlock;
+private slots:
+    void start();  // 啟動伺服器
+    void acceptConnection();  // 接受來自客戶端的連線
+    void updateServerProgress();  // 更新接收檔案的進度
+    void displayError(QAbstractSocket::SocketError socketError);  // 顯示錯誤訊息
+
+private:
+    QTcpServer tcpServer;  // TCP伺服器
+    QTcpSocket *tcpServerConnection;  // 用來處理與客戶端的連線
+    QFile *localFile;  // 用來儲存接收到的檔案
+    QProgressBar *serverProgressBar;  // 顯示接收進度的進度條
+    QLabel *serverStatusLabel;  // 顯示伺服器狀態的標籤
+    QPushButton *startButton;  // 啟動伺服器按鈕
+    QPushButton *quitButton;  // 退出按鈕
+    QLineEdit *ipLineEdit;  // 用戶輸入IP地址
+    QLineEdit *portLineEdit;  // 用戶輸入端口號
+    QDialogButtonBox *buttonBox;  // 按鈕框
+    qint64 totalBytes;  // 總的檔案大小
+    qint64 byteReceived;  // 已接收到的檔案大小
+    qint64 fileNameSize;  // 檔名長度
+    QString fileName;  // 檔名
+    QByteArray inBlock;  // 存放讀取資料的緩衝區
 };
 
 #endif // TCPFILESERVER_H
